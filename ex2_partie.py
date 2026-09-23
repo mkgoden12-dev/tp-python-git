cf25a279dcdef5c2fe3999a41e608457b8928133
GMK <mkgoden12@gmail.com>
2026-09-23 10:53:49 +0100

    feat: ajout de l'exercice 2 sur la parite

---
+++ ex2_partie.py
@@ -0,0 +1,8 @@
+# ex2_parite.py
+
+nombre = int(input("Entrez un nombre entier : "))
+
+if nombre % 2 == 0:
+ print(f"Le nombre {nombre} est pair.")
+else:
+ print(f"Le nombre {nombre} est impair.")
